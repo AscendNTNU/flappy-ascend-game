4223b3861f00ae33ae543fb35e979d40032593b0
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-08-12 18:09:54 +0200

    Added game loop with time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import Player from './components/Player'
 
 let canvas = document.getElementById('canvas')
+let cw = canvas.width
+let ch = canvas.height
+
+let ctx = canvas.getContext('2d')
+
+let startTime = 0
+function loop (time) {
+    if (!startTime) startTime = time
+    let progress = time - startTime
+
+    update(progress)
+
+    window.requestAnimationFrame(loop)
+}
+
+function init () {
+    window.requestAnimationFrame(loop)
+}
+
+function update (progress) {
+    ctx.fillStyle = 'gray'
+    ctx.fillRect(0, 0, cw, ch)
+
+}
+
+init()
