75678b16c82367ac8a387ab484979063f10397d9
niklasmh <nikasmh@hotmail.com>
2017-08-16 23:15:39 +0200

    Adding babel and some css to make ios feeling better

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,16 @@ module.exports = {
         enforce: 'pre',
         test: /\.js$/,
         loader: 'source-map-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
       }
     ]
   }
